1f91d2e52c4ab1bc3327bdb790dd1e988bde5da6
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-02-24 23:35:34 -0500

    Work Session 4REAL

---
+++ Assets/__Scripts/Enemy_6.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_6 : MonoBehaviour
+{
+    public float speed = .01f;
+    public ParticleSystem explosionParticle;
+
+    void Update()
+    {
+        if(Hero.S != null)
+            transform.position = Vector3.Lerp(transform.position, Hero.S.gameObject.transform.position, speed*Time.deltaTime);   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponentInParent<Hero>() != null)
+        {
+            Detonate();
+            //DamageHero();
+            Destroy(gameObject);
+        }
+    }
+
+    private void Detonate()
+    {
+        ParticleSystem explosionParticleClone = Instantiate(explosionParticle, transform.position, Quaternion.identity);
+    }
+}
